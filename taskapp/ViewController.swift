afc35fe0fad6a3c7ca181773c94792c1bef8cefe
kazunobu.ide <idekazunobu@gmail.com>
2021-01-06 16:40:03 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by 井手　和宣 on 2021/01/06.
+//  Copyright © 2021 kazunobu.ide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
