3d168128fdeab7418d9ac46b3cb36b451bc1a5d0
kazunobu.ide <idekazunobu@gmail.com>
2021-01-06 20:20:33 +0900

    エラーが出る

---
+++ taskapp/Task.swift
@@ -0,0 +1,28 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 井手　和宣 on 2021/01/06.
+//  Copyright © 2021 kazunobu.ide. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // タイトル
+    @objc dynamic var title = ""
+
+    // 内容
+    @objc dynamic var contents = ""
+
+    // 日時
+    @objc dynamic var date = Date()
+
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
